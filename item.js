8d5884096761fd76eed0b6ff5df4d5c22f84072a
achrefmechergui <achreffcb123achref@gamil.com>
2023-12-29 14:58:55 +0100

    add book , mode,navbar

--- item.js
+++ item.js
@@ -77,7 +77,7 @@ var newBook = `
         <h3></h3>
         <p class="price"></p>
         <p class="price"></p>
-        <p><button>buy</button></p>
+        <p><button >buy</button></p>
       </div>`;
 // new book is a variable witch i puted an empty card bring it from w3school to put into it the information of the books
 
@@ -89,12 +89,31 @@ const bookPriceInput = document.getElementById("price"); //the input of book pri
 const bookIdInput = document.getElementById("id"); // the input of the id
 const bookImageInput = document.getElementById("photo"); // the input of the photo
 const cardContainer = document.getElementById("cardcontainer"); // the div witch contains the books that i made it
+const buttonaddacceuil = document.getElementById("homeadd");
+const addform = document.getElementById("addBookForm");
+const closebutton = document.getElementById("btnclose");
+buttonaddacceuil.addEventListener("click", function () {
+  addform.style.display = "block";
+});
+closebutton.addEventListener("click", function () {
+  addform.style.display = "none";
+});
 
 var myBookstore = bookstore(); // give my oop function an instance
 
+function makethenewcardswork() {
+  var buttons = document.querySelectorAll(".btnbuy");
+  each(buttons, function (element) {
+    element.addEventListener("click", function () {
+      smallvisacontainer.style.display = "block";
+      bigvisacontainer.style.display = "block";
+    });
+  });
+}
+
 //function to add new books :
 function addnewBook(event) {
-  event.preventDefault(); // i use preventDefault() to have a new function each time i add a book without actualisate the page
+  // event.preventDefault(); // i use preventDefault() to have a new function each time i add a book without actualisate the page
 
   const name = bookNameInput.value; // name will contain the value witch i will write it in the input of name
   const price = bookPriceInput.value; // price will contain the value witch i will write it in the input of price
@@ -108,11 +127,11 @@ function addnewBook(event) {
   const newBookElement = document.createElement("div"); // make new div to put my newcard book into it
   newBookElement.classList.add("card"); // give him the same class that i already used in my css doc
   newBookElement.innerHTML = `
-    <img src="${image}" style="width: 100%" />
+    <img class="bookimage" src="${image}" style="width: 100%" />
     <h1>${name}</h1>
     <p class="price">${price}</p>
     <p class="price">${id}</p>
-    <p><button>buy</button></p>
+    <p><button class="btnbuy">buy</button></p>
   `; // then i put my card in the div that i created using .innerhtml with putting the value of the inputs
 
   cardContainer.appendChild(newBookElement);
@@ -122,18 +141,38 @@ function addnewBook(event) {
   bookPriceInput.value = "";
   bookIdInput.value = "";
   bookImageInput.value = "";
+  makethenewcardswork();
 }
 
 // add an eventlistner to the button  when i click into it the function that i made it will do the work
 addBookButton.addEventListener("click", addnewBook);
 
-const deleteBookButton = document.getElementById("btndelete");
-const deleteBookInput = document.getElementById("deletebook");
+var buybutton = document.querySelectorAll(".btnbuy");
+var bigvisacontainerarr = document.querySelectorAll("#bigvisacontainer");
+var bigvisacontainer = document.getElementById("bigvisacontainer");
+each(buybutton, function (element) {
+  element.addEventListener("click", function () {
+    smallvisacontainer.style.display = "block";
+    bigvisacontainer.style.display = "block";
+  });
+});
+var visabutton = document.querySelectorAll("#btnconfirm");
+var message = document.getElementById("msg");
+var smallvisacontainer = document.getElementById("smallvisacontainer");
+each(visabutton, function (element) {
+  element.addEventListener("click", function () {
+    smallvisacontainer.style.display = "none";
+    message.style.display = "block";
+    setTimeout(function () {
+      message.style.display = "none";
+      bigvisacontainer.style.display = "none";
+    }, 3000);
+  });
+});
 
-function deleteBook() {
-  const bookIdToDelete = deleteBookInput.value;
-
-  myBookstore.remove(bookIdToDelete);
-}
-deleteBookButton.addEventListener("click", deleteBook);
-
+var mode = document.getElementById("mode");
+var nav = document.getElementById("nav");
+mode.addEventListener("click", function () {
+  cardContainer.classList.toggle("newmode");
+  nav.classList.toggle("newnavbar");
+});
